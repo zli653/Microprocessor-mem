5982059631681d50d98a86edcfe9cbc5f614d6f5
Ethan Young <eyoung@snares-02.cs.wisc.edu>
2019-04-13 12:48:31 -0500

    More progress

--- project/cache_direct/verilog/cache_fsm.v
+++ project/cache_direct/verilog/cache_fsm.v
@@ -27,24 +27,36 @@ module cache_fsm(
 	output fs_err;
 	
 	// Internal wires
-	wire [3:0] state,next_state;
+	reg [3:0] state,next_state;
+	reg f_err;
 
 	/*
 	* _______STATE_KEY______
 	*
-	* 	IDLE : 00000
-	* 	
+	* 	IDLE :		0000
+	* 	COMP_WRITE : 	0001
+	* 	COMP_READ : 	0010
+	* 	EVICT_1 :	0011
+	* 	EVICT_2 :	0100
+	* 	EVICT_3 :	0101
+	* 	EVICT_4 :	0110
+	* 	EVICT_5 :	0111
+	* 	MEM_ACC_1 : 	1000
+	* 	MEM_ACC_2 :	1001
+	* 	MEM_ACC_3 :	1010
+	* 	MEM_ACC_4 :	1011
+	* 	MEM_ACC_5 :	1100
+	* 	MEM_ACC_6 :	1101
 	* 	
 	*/
 
 
 	// Holds state for state machine
 	dff state_ff [3:0] (.q(next_state), .d(state), .clk(clk), .rst(rst));
+	
+	assign fs_err = c_err | m_err | f_err;
 
-	assign fs_err = c_err | m_err;
-
-	always@(*) begin
-		
+	always@(*) begin	
 		// Set to default, only change necessary
 		fm_addr 	= 16'd0;
 		fm_data_in	= 16'd0;
@@ -62,26 +74,67 @@ module cache_fsm(
 		fs_done		= 1'b0;
 		fs_stall	= 1'b0;
 		fs_cachehit	= 1'b0;
-		
+		f_err		= 1'b0;
+
+		next_state = 4'd0;
 		case(state)
 			4'b0000://IDLE
 			begin
 				case({write, read}):
 					2'b10:
 					begin
+						// go to comp write
+						next_state = 4'b0001;
+						fc_comp = 1'b1;
+						fc_write = 1'b1;
+						fc_enable = 1'b1;
+						fc_offset = addr[2:0];
+						fc_index = addr[10:3];
+						fc_tag_in = addr[15:11];
 					end
 					2'b01:
 					begin
+						// go to comp read
+						next_state = 4'b0010
+						fc_comp = 1'b1;
+						fc_write = 1'b0;
+						fc_enable = 1'b1;
+						fc_offset = addr[2:0];
+						fc_index = addr[10:3];
+						fc_tag_in = addr[15:11];
+
 					end
 					2'b00:
 					begin
+						// stay in IDLE
+					end
+					2'b11: // read and write at the same time, print error message      
+					begin
+						f_err = 1'b1;
+					end
+					default: // never reached, print error message
+					begin
+						f_err = 1'b1;
 					end	
 				endcase	
 			end
-			
+
+			4'b0001://COMP_WRITE
+			begin
+				case({c_hit, c_valid, c_dirty}):
+					3'b1:
+			end	
 			default://DEFAULT to IDLE (Never reached)
 			begin
-				//TODO
+				f_err = 1'b1;
+			end
+			
+			4'b0010://COMP_READ
+			begin
+			end	
+			default://DEFAULT to IDLE (Never reached)
+			begin
+				f_err = 1'b1;
 			end
 		endcase
 	end	
