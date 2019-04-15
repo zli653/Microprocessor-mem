e086090349da5cf1e9f95cc8516c3724c374ed84
Zeming Li <zli653@wisc.edu>
2019-04-15 16:59:08 -0500

    working on it

--- project/cache_assoc/verilog/cache_fsm.v
+++ project/cache_assoc/verilog/cache_fsm.v
@@ -46,7 +46,7 @@ module cache_fsm(
 	wire curr_c_valid_2, curr_c_dirty_2, curr_c_hit_2;
 	wire idle;
 
-	assign idle = (state_int == 4'b0000)&(next_state_int == 4'b0001 | next_state_int == 4'b0010);
+	assign idle = (state_int == 4'b0001 | state_int == 4'b0010);
 	/*
 	* _______STATE_KEY______
 	*
@@ -96,12 +96,8 @@ module cache_fsm(
 	dff c_valid_f_2 (.q(curr_c_valid_2), .d(c_valid_2), .clk(clk), .rst(rst));
 	
 	assign f_stall = (m_stall&~fs_cachehit) | ((write | read) & ~fs_done);
-	always@(c_rep)
-	begin
-		$display("c_rep: %d", c_rep);
-	end
 		
-	wire curr_rand, rand, c_sel, c_hit, c_hit_way_num;
+	wire curr_rand, rand, c_sel, c_hit;
 	wire fc_enable, fc_comp, fc_write, fc_valid_in;
 	wire [2:0] fc_offset;
 	wire [4:0] fc_tag_in;
@@ -115,60 +111,69 @@ module cache_fsm(
 	// c_sel is 1, choose way 2
 	assign c_err = c_sel ? c_err_2 : c_err_1;
 	assign c_sel = (idle) ? (~curr_c_valid_1 ? 1'b0 : 
-			curr_c_valid_2 ? curr_rand : 1'b1) :
-			curr_c_sel;
+			(curr_c_valid_2 ? curr_rand : 1'b1) ) :
+				// TODO: only for optimization
+				// both are valid, cannot be both hit
+				// both are valid, if one is hit, choose the one is hit
+				// both are valid, if no hit
+				// 							if both dirty, random
+				// 							if one dirty, pick the other
+				// 							if no dirty, whatever
+				// (curr_c_hit ? curr_c_hit_2: curr_rand) : 1'b1) :
+				curr_c_sel;
 	dff c_sel_f_2 (.q(curr_c_sel), .d(c_sel), .clk(clk), .rst(rst));
 
 	// Did hit occur?	
-	assign c_hit = curr_c_hit_1 | curr_c_hit_2;
-	// Which cache had hit (1 = way 2)
-	assign c_hit_way_num = c_hit & curr_c_hit_2;
-	// If hit : pick correct data out. 
+	// // Which cache had hit (1 = way 2)
+	// assign c_hit_way_num = c_hit & curr_c_hit_2;
+	// // If hit : pick correct data out. 
 	
-	assign c_rep = c_hit ? c_hit_way_num : c_sel;	
-	assign curr_c_tag_out = c_rep ? curr_c_tag_out_2 : curr_c_tag_out_1;
-	assign curr_c_data_out = c_rep ? curr_c_data_out_2 : curr_c_data_out_1;
+	// assign c_rep = c_hit ? c_hit_way_num : c_sel;	
+	// assign curr_c_tag_out = c_rep ? curr_c_tag_out_2 : curr_c_tag_out_1;
+	// assign curr_c_data_out = c_rep ? curr_c_data_out_2 : curr_c_data_out_1;
 					
-	assign curr_c_valid  = c_rep ? curr_c_valid_2 : curr_c_valid_1;
-	assign curr_c_dirty = c_sel ? curr_c_dirty_2 : curr_c_dirty_1;
+	// assign curr_c_valid  = c_rep ? curr_c_valid_2 : curr_c_valid_1;
+	// assign curr_c_dirty = c_sel ? curr_c_dirty_2 : curr_c_dirty_1;
 	
-	assign fc_data_in_1 = ~c_rep|idle ? fc_data_in: 16'b0;
-	assign fc_index_1 = ~c_rep|idle ? fc_index: 8'b0;		
-	assign fc_tag_in_1 = ~c_rep|idle ? fc_tag_in : 5'b0;		
-	assign fc_offset_1 = ~c_rep|idle ? fc_offset : 3'b0;		
-	assign fc_enable_1 = ~c_rep|idle ? fc_enable : 1'b0;		
-	assign fc_comp_1 = ~c_rep|idle ?  fc_comp : 1'b0;		
-	assign fc_write_1 = ~c_rep|idle ? fc_write : 1'b0;		
-	assign fc_valid_in_1 = ~c_rep|idle ? fc_valid_in : 1'b1;	
+	wire curr_c_hit_valid, curr_c_hit_dirty;
+	wire [15:0] curr_c_return_data_out;
+	assign c_hit = curr_c_hit_1 ^ curr_c_hit_2;
+	assign curr_c_valid  = (c_hit & curr_c_hit_valid) ? curr_c_hit_valid : 
+							c_sel ? curr_c_valid_2 : curr_c_valid_1;
+	assign curr_c_dirty = (c_hit & curr_c_hit_valid) ? curr_c_hit_dirty : 
+							c_sel ? curr_c_valid_2 : curr_c_valid_1;
+	assign curr_c_tag_out = c_sel ? curr_c_tag_out_2 : curr_c_tag_out_1;
+	assign curr_c_data_out = (c_hit & curr_c_hit_valid) ? curr_c_return_data_out :
+								c_sel ? curr_c_data_out_2 : curr_c_data_out_1;
+	
+	assign curr_c_hit_valid= (curr_c_hit_2 ? curr_c_valid_2 : curr_c_valid_1);
+	assign curr_c_hit_dirty= (curr_c_hit_2 ? curr_c_dirty_2 : curr_c_dirty_1);
+	// assign curr_c_return = curr_c_hit & curr_c_hit_valid;
+	assign curr_c_return_data_out = curr_c_hit_2 ? curr_c_data_out_2 : curr_c_data_out_1;
+	// assign curr_c_data_out = curr_c_return ? curr_c_return_data_out : 
+	// 				curr_c_rep ? curr_c_data_out_2 : curr_c_data_out_1;
+	// assign curr_c_valid = curr_c_hit ? curr_c_hit_valid : 
+	// 					(curr_c_sel ? curr_c_valid_2 : curr_c_valid_1);
 
-	assign fc_data_in_2 = c_rep|idle ? fc_data_in : 16'b0;
-	assign fc_index_2 = c_rep|idle ? fc_index : 16'b0;		
-	assign fc_tag_in_2 = c_rep|idle ? fc_tag_in : 16'b0;		
-	assign fc_offset_2 = c_rep|idle ? fc_offset : 16'b0;		
-	assign fc_enable_2 = c_rep|idle ? fc_enable : 1'b0;		
-	assign fc_comp_2 = c_rep|idle ? fc_comp : 1'b0;		
-	assign fc_write_2 = c_rep|idle ? fc_write : 1'b0;		
-	assign fc_valid_in_2 = c_rep|idle ? fc_valid_in : 1'b1;	
 
+	assign fc_data_in_1 = ~c_sel|idle ? fc_data_in: 16'b0;
+	assign fc_index_1 = ~c_sel|idle ? fc_index: 8'b0;		
+	assign fc_tag_in_1 = ~c_sel|idle ? fc_tag_in : 5'b0;		
+	assign fc_offset_1 = ~c_sel|idle ? fc_offset : 3'b0;		
+	assign fc_enable_1 = ~c_sel|idle ? fc_enable : 1'b0;		
+	assign fc_comp_1 = ~c_sel|idle ?  fc_comp : 1'b0;		
+	assign fc_write_1 = ~c_sel|idle ? fc_write : 1'b0;		
+	assign fc_valid_in_1 = ~c_sel|idle ? fc_valid_in : 1'b1;	
 
-/*	assign fc_data_in_1 = c_sel ? 16'b0 : fc_data_in;
-	assign fc_index_1 = c_sel ? 8'b0 : fc_index;		
-	assign fc_tag_in_1 = c_sel ? 5'b0 : fc_tag_in;		
-	assign fc_offset_1 = c_sel ? 3'b0  : fc_offset;		
-	assign fc_enable_1 = c_sel ? 1'b0  : fc_enable;		
-	assign fc_comp_1 = c_sel ? 1'b0  : fc_comp;		
-	assign fc_write_1 = c_sel ? 1'b0  : fc_write;		
-	assign fc_valid_in_1 = c_sel ? 1'b0  : fc_valid_in;	
+	assign fc_data_in_2 = c_sel|idle ? fc_data_in : 16'b0;
+	assign fc_index_2 = c_sel|idle ? fc_index : 16'b0;		
+	assign fc_tag_in_2 = c_sel|idle ? fc_tag_in : 16'b0;		
+	assign fc_offset_2 = c_sel|idle ? fc_offset : 16'b0;		
+	assign fc_enable_2 = c_sel|idle ? fc_enable : 1'b0;		
+	assign fc_comp_2 = c_sel|idle ? fc_comp : 1'b0;		
+	assign fc_write_2 = c_sel|idle ? fc_write : 1'b0;		
+	assign fc_valid_in_2 = c_sel|idle ? fc_valid_in : 1'b1;	
 
-	assign fc_data_in_2 = c_sel ? fc_data_in : 16'b0;
-	assign fc_index_2 = c_sel ? fc_index : 16'b0;		
-	assign fc_tag_in_2 = c_sel ? fc_tag_in : 16'b0;		
-	assign fc_offset_2 = c_sel ? fc_offset : 16'b0;		
-	assign fc_enable_2 = c_sel ? fc_enable : 1'b0;		
-	assign fc_comp_2 = c_sel ? fc_comp : 1'b0;		
-	assign fc_write_2 = c_sel ? fc_write : 1'b0;		
-	assign fc_valid_in_2 = c_sel ? fc_valid_in : 1'b0;	
-*/
 	cache_fsm_wrapper fsm (
 	                // Inputs
 	                .addr(curr_addr),.data_in(curr_data_in),.read(curr_read),.write(curr_write),.rst(rst),
