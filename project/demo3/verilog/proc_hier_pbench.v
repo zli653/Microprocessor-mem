6cab274ff62518a64c714372536341ae44ba5fe7
Ethan Young <eyoung@snares-02.cs.wisc.edu>
2019-04-22 17:33:00 -0500

    Updated hit rate calc

--- project/demo3/verilog/proc_hier_pbench.v
+++ project/demo3/verilog/proc_hier_pbench.v
@@ -213,22 +213,22 @@ module proc_hier_pbench();
    // Data read from memory for memory reads (16 bits)
 
    // new added 05/03
-   assign ICacheReq = DUT.p0.MemoryStage.MemOut;
+   assign ICacheReq = DUT.p0.FetchStage.imem.Rd & DUT.p0.FetchStage.imem.Done ;
    //THEIRS: assign ICacheReq = DUT.p0.readData;
    // Signal indicating a valid instruction read request to cache
    // Above assignment is a dummy example
    
-   assign ICacheHit = DUT.p0.MemoryStage.MemOut;
+   assign ICacheHit = DUT.p0.FetchStage.CacheHit;
    //THEIRS: assign ICacheHit = DUT.p0.readData;
    // Signal indicating a valid instruction cache hit
    // Above assignment is a dummy example
 
-   assign DCacheReq = DUT.p0.MemoryStage.MemOut;
+   assign DCacheReq = (DUT.p0.MemoryStage.mmem.Rd | DUT.p0.MemoryStage.mmem.Wr) & DUT.p0.MemoryStage.mmem.Done ;
    //THEIRS: assign DCacheReq = DUT.p0.readData;
    // Signal indicating a valid instruction data read or write request to cache
    // Above assignment is a dummy example
    //    
-   assign DCacheHit = DUT.p0.MemoryStage.MemOut;
+   assign DCacheHit = DUT.p0.MemoryStage.CacheHit;
    //THEIRS: assign DCacheHit = DUT.p0.readData;
    // Signal indicating a valid data cache hit
    assign Halt = DUT.p0.FetchStage.Halt;
